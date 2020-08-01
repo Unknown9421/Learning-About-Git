675558c8076539acbbe63e3e30beebedc657cdb9
Nguyễn Đức Hoàng <ndhoang9421@gmail.com>
2020-08-01 11:25:08 +0700

    Learning Testing From Hoang Branch

--- Learning Testing/test.py
+++ Learning Testing/test.py
@@ -17,7 +17,7 @@ class GetFullNameTestCase(unittest.TestCase):
 
     def test_first_last__name(self):
         result = get_full_name('Nguyễn', 'Hoàng', middle_name='Đức')
-        self.assertEqual(result, "Nguyễn Hoàng")
+        self.assertEqual(result, "Nguyễn Ha Hoàng")
 
     def test_first_last_middle_name(self):
         result = get_full_name('Nguyễn', 'Hoàng', middle_name='Đức')
